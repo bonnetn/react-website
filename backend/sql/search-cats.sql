0e44a763359d0c0f842071a497e1f6efb57a292e
Nicolas Bonnet <mail@nicolasbon.net>
2023-01-07 20:22:43 +0100

    Update

--- backend/sql/search-cats.sql
+++ backend/sql/search-cats.sql
@@ -1,3 +1,43 @@
+-- NOTE: This files contains two functions:
+-- 1. search_cats_forward: Which searches and returns the first N results.
+-- 2. search_cats_backward: Same, but returns the N last results.
+--
+-- This allows PostgreSQL to efficiently retrieve the rows in both scenario.
+-- (using a index scan, or a reverse index scan).
+
+-- Search the cats table.
+-- WARNING: This function is not paginated and may be very expensive.
+CREATE FUNCTION pg_temp.search_cats_unpaginated(query TEXT, after INT, before INT)
+    RETURNS TABLE
+            (
+                id         INT,
+                uuid       TEXT,
+                name       TEXT,
+                age        INT,
+                owner_uuid TEXT,
+                owner_name TEXT
+            )
+AS
+$$
+SELECT cats.id     AS id,
+       cats.uuid   AS uuid,
+       cats.name   AS name,
+       cats.age    AS age,
+       owners.uuid AS owner_uuid,
+       owners.name AS owner_name
+
+FROM cats
+         INNER JOIN owners ON owners.id = cats.owner_id
+
+WHERE cats.name ILIKE '%' || query || '%'
+  AND (after IS NULL OR after < cats.id)
+  AND (before IS NULL OR cats.id < before)
+
+$$
+    STABLE
+    LANGUAGE SQL;
+
+-- Search the cats table and return the first N results.
 CREATE FUNCTION pg_temp.search_cats_forward(query TEXT, first INT, after INT, before INT)
     RETURNS TABLE
             (
@@ -10,34 +50,16 @@ CREATE FUNCTION pg_temp.search_cats_forward(query TEXT, first INT, after INT, be
             )
 AS
 $$
-WITH
-    -- Main query:
-    search_results AS (SELECT cats.id     AS id,
-                              cats.uuid   AS uuid,
-                              cats.name   AS name,
-                              cats.age    AS age,
-                              owners.uuid AS owner_uuid,
-                              owners.name AS owner_name
-
-                       FROM cats
-                                INNER JOIN owners ON owners.id = cats.owner_id
-
-                       WHERE cats.name ILIKE '%' || query || '%')
-
-    -- Only select the "first" n elements starting from the cursor "after".
+-- Only select the "first" n elements starting from the cursor "after".
 SELECT *
-FROM search_results
-WHERE (after IS NULL OR after < id)
-  AND (before IS NULL OR id < before)
+FROM pg_temp.search_cats_unpaginated(query, after, before)
 ORDER BY id
 LIMIT first
 $$
     STABLE
     LANGUAGE SQL;
 
--- Search the cats table.
--- If "first" or "last" is NULL, it assumes +inf.
--- If "before" or "after" is NULL, it ignores the cursor constraint.
+-- Search the cats table and return the last N results..
 CREATE FUNCTION pg_temp.search_cats_backward(query TEXT, last INT, after INT, before INT)
     RETURNS TABLE
             (
@@ -50,26 +72,10 @@ CREATE FUNCTION pg_temp.search_cats_backward(query TEXT, last INT, after INT, be
             )
 AS
 $$
-WITH
-    -- Main query:
-    search_results AS (SELECT cats.id     AS id,
-                              cats.uuid   AS uuid,
-                              cats.name   AS name,
-                              cats.age    AS age,
-                              owners.uuid AS owner_uuid,
-                              owners.name AS owner_name
-
-                       FROM cats
-                                INNER JOIN owners ON owners.id = cats.owner_id
-
-                       WHERE cats.name ILIKE '%' || query || '%')
-
-    -- Only select the "last" n elements ending on the cursor "before".
+-- Only select the "last" n elements ending on the cursor "before".
 SELECT *
 FROM (SELECT *
-      FROM search_results
-      WHERE (after IS NULL OR after < id)
-        AND (before IS NULL OR id < before)
+      FROM pg_temp.search_cats_unpaginated(query, after, before)
       ORDER BY id DESC
       LIMIT last) AS results
 ORDER BY id
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -18,9 +18,8 @@ const repository = new PostgresRepository(pgPool);
 const handler = new Handler(repository);
 const resolvers = handler.resolvers;
 
-console.log(
-  await repository.searchCats("", { state: "last", value: 42 }, 10, null)
-);
+await repository.searchCats("", { state: "last", value: 42 }, 10, null);
+await repository.searchCats("", { state: "first", value: 42 }, 10, null);
 console.log(await repository.fetchCat("fdec4fc8-a39c-4cf7-8bfd-28e305a33c1b"));
 
 const typeDefs = readFileSync("./schema.graphql", { encoding: "utf-8" });
